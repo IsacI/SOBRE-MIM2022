4961ffbd0124ab03d47b77f59a111d122dff3c0e
Isac Narcizo Baroni Bazzani <106942461+IsacI@users.noreply.github.com>
2022-11-19 08:14:13 -0300

    Add files via upload

---
+++ JavaScript_3.jsx
@@ -0,0 +1,10 @@
+const a = prompt('digite um valor para A');
+const b = prompt('digite um valor para B');
+
+let soma = a + b;
+soma = soma + 10;
+
+console.log(soma);
+
+const nome = prompt('Digite seu nome');
+alert('Olá' + nome);
